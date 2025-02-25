af04cfc9b494ec93f567c43314a4c4398331f501
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-02-25 14:36:44 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,8 +1,7 @@
-chrome.runtime.onInstalled.addListener(() =>
-  chrome.contentSettings.javascript.get({ primaryUrl: "https://www.wikipedia.org" }, e =>
-    e.setting == "allow" && chrome.contentSettings.javascript.set({
+chrome.runtime.onInstalled.addListener(async () =>
+  (await (chrome.contentSettings.javascript.get({ primaryUrl: "https://www.wikipedia.org" }))).setting == "allow" &&
+    chrome.contentSettings.javascript.set({
       primaryPattern: "https://*.wikipedia.org/*",
       setting: "block"
     })
-  )
 );
